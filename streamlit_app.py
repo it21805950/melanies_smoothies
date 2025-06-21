0a8529a38b00d1903fdd5e2444ad2945c19907bb
Naduni Athukorala <110513688+it21805950@users.noreply.github.com>
2025-06-21 14:17:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,20 @@ if ingredients_list:
 
 #new section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
+import pandas as pd
+
+# Get JSON response
+data = smoothiefroot_response.json()
+
+# Convert to DataFrame
+sf_df = pd.DataFrame([data])  # wrap in list to form a row
+
+# Display as table
+st.subheader("🍉 Watermelon Nutrition Information")
+st.dataframe(data=sf_df, use_container_width=True)
 
